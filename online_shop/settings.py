4d40bd05e1de1a5f8156748a2a19c310b7ebf397
Mehrawbmh <mehrab@mylaptop>
2022-02-15 22:50:58 +0330

    Config: multi language website

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -109,6 +110,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+LANGUAGES = (('en', 'English'), ('fa', 'Farsi'))
+LOCALE_PATHS = (BASE_DIR / 'locale',)
 
 TIME_ZONE = 'Asia/Tehran'
 
