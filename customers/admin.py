66b1423792ba9c3cb481581ada33f503bee3400a
Mehrawbmh <mehrab@mylaptop>
2022-02-20 15:06:28 +0330

    Add new version of ERD

--- customers/admin.py
+++ customers/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
 from customers.models import Customer, Address
 
 admin.site.register(Address)
-admin.site.register(Customer, UserAdmin)
+admin.site.register(Customer)
