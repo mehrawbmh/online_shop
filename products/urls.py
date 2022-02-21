b1470aca24fc871894255987108558628d392bef
Mehrawbmh <mehrab@mylaptop>
2022-02-21 22:54:19 +0330

    Footer and Navigationbar styles for index page

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import ProductListView
 urlpatterns = [
-    path('', ProductListView.as_view())
+    path('', ProductListView.as_view(), name='index'),
+    path('', ProductListView.as_view(), name='login')
 ]
