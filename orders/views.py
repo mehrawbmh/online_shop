e3a7cc832691c7dcdaebd3c78779bf758e12aa61
Mehrawbmh <mehrab@mylaptop>
2022-03-08 18:09:01 +0330

    Last commit for phase3: show cart items for anonymous user in basket.html

--- orders/views.py
+++ orders/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.views import View
+
+from products.models import Product
 from .models import CartItem, Cart
 
 
 class CartView(View):
 
     def get(self, request):
-        data = dict() #context!
+        data = dict()  # context!
         if request.user.is_authenticated:
             cart = self.request.user.customer.cart_set.last()
+            cart: Cart
             data = {'cart': cart}
             if cart:
                 data['items'] = CartItem.objects.filter(cart=cart)
@@ -16,7 +19,18 @@ class CartView(View):
                 data['items'] = None
         else:
             data['cart'] = None
-            print(request.COOKIES)
+            items = list()
+            sum = 0
+            for key, value in self.request.COOKIES.items():
+                key: str
+                if key.startswith('prod'):
+                    prod_id = int(key[4:])
+                    product = Product.objects.get(id=prod_id)
+                    items.append((product, value))
+                    sum += int(product.final_price) * int(value)
+            print('sum:', sum)
+            data['items'] = items
+            data['totalprize'] = str(sum)
 
         return render(request, 'orders/basket.html', data)
 
